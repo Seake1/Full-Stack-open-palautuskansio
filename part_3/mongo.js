5b281395d47df9378b2221f268a0d2b9eef043ec
Christian <79089942+Seake1@users.noreply.github.com>
2024-10-30 21:07:01 +0200

    Update mongo.js

--- part_3/mongo.js
+++ part_3/mongo.js
@@ -9,7 +9,7 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4] ? process.argv[4] : ''
 
-const url = `mongodb+srv://user:${password}@cluster0.4eigpqj.mongodb.net/phonebook?retryWrites=true&w=majority`
+const url = `mongodb+srv://user:${password}@cluster0.4aiptg.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
